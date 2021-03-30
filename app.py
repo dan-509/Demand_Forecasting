996ed262a9ce9f562de86f07f49fced6141cfb15
dan-509 <72122033+dan-509@users.noreply.github.com>
2021-03-30 18:54:57 +1100

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,5 @@ app = Flask(__name__)
 datetime = datetime.datetime.now()
 @app.route("/")
 def hello():
-    if datetime.minute%5 == 0:
-        x = {'Demands': [1,2,3,4,5,6,7,8,9]}
-        return json.dumps(x)
+    x = {'Demands': [1,2,3,4,5,6,7,8,9]}
+    return json.dumps(x)
