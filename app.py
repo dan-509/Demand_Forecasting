058a45c49132d17a771fe4cb76e0a80e11706d0e
dan-509 <72122033+dan-509@users.noreply.github.com>
2021-03-30 19:24:04 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
-
+import datetime
+datetime
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    x = {'Demands': [1,2,3,4,5,6,7,8,9]}
-    return x
+    if datetime.minute%2 == ):
+        x = {'Demands': [1,2,3,4,5,6,7,8,9]}
+        return x
