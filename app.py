1fc2fa25750be21deae528a1bfaf640525ebec24
dan-509 <72122033+dan-509@users.noreply.github.com>
2021-03-30 18:59:51 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask
-import datetime
-import json
+
 app = Flask(__name__)
 datetime = datetime.datetime.now()
 @app.route("/")
 def hello():
     x = {'Demands': [1,2,3,4,5,6,7,8,9]}
-    return json.dumps(x)
+    return x
